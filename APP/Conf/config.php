dbd2c12c40346723707dcd1e42fb61c1baf065b7
plsxysam <418228079@qq.com>
2015-09-06 23:36:34 +0800

    载入Image类，完成登录，退出功能，添加验证码配置

--- APP/Conf/config.php
+++ APP/Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-	
+
 	'DB_HOST' => '127.0.0.1',
 	'DB_USER' => 'root',
 	'DB_PWD' => '123456',
@@ -12,7 +12,9 @@ return array(
 	'DEFAULT_GROUP' => 'Index',
 	'APP_GROUP_MODE' => 1,
 	'APP_GROUP_PATH' => 'Modules',
-	
+
+	//加载独立配置项文件
+	'LOAD_EXT_CONFIG' => 'verify',
 	
 );
 ?>
--- APP/Modules/Admin/Action/IndexAction.class.php
+++ APP/Modules/Admin/Action/IndexAction.class.php
@@ -9,6 +9,11 @@ Class IndexAction extends CommonAction{
 		$this->display();
 	}
 
-	
+	//退出登录
+	public function logout(){
+		session_unset();
+		session_destroy();
+		$this->redirect(GROUP_NAME . '/Login/index');
+	}
 }
  ?>
